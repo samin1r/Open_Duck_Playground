23bba99106ab3fe330117dcebc4fa5e520483cb1
apirrone <awanegenebayo@gmail.com>
2025-03-05 12:18:08 +0100

    go_bdx

---
+++ playground/go_bdx/constants.py
@@ -0,0 +1,85 @@
+# Copyright 2025 DeepMind Technologies Limited
+# Copyright 2025 Antoine Pirrone - Steve Nguyen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Constants for Go_bdx (headless version) . (based on Berkeley Humanoid)"""
+
+from etils import epath
+
+
+ROOT_PATH = epath.Path(__file__).parent
+FEET_ONLY_FLAT_TERRAIN_XML = ROOT_PATH / "xmls" / "scene_mjx_flat_terrain.xml"
+FEET_ONLY_ROUGH_TERRAIN_XML = ROOT_PATH / "xmls" / "scene_mjx_rough_terrain.xml"
+
+
+def task_to_xml(task_name: str) -> epath.Path:
+    return {
+        "flat_terrain": FEET_ONLY_FLAT_TERRAIN_XML,
+        "rough_terrain": FEET_ONLY_ROUGH_TERRAIN_XML,
+    }[task_name]
+
+
+FEET_SITES = [
+    "left_foot",
+    "right_foot",
+]
+
+LEFT_FEET_GEOMS = [
+    "left_foot_pad",
+]
+
+RIGHT_FEET_GEOMS = [
+    "right_foot_pad",
+]
+
+HIP_JOINT_NAMES = [
+    "left_hip_yaw",
+    "left_hip_roll",
+    "left_hip_pitch",
+    "right_hip_yaw",
+    "right_hip_roll",
+    "right_hip_pitch",
+]
+
+KNEE_JOINT_NAMES = [
+    "left_knee",
+    "right_knee",
+]
+
+# There should be a way to get that from the mjModel...
+JOINTS_ORDER_NO_HEAD = [
+    "left_hip_yaw",
+    "left_hip_roll",
+    "left_hip_pitch",
+    "left_knee",
+    "left_ankle",
+    "right_hip_yaw",
+    "right_hip_roll",
+    "right_hip_pitch",
+    "right_knee",
+    "right_ankle",
+]
+
+FEET_GEOMS = LEFT_FEET_GEOMS + RIGHT_FEET_GEOMS
+
+FEET_POS_SENSOR = [f"{site}_pos" for site in FEET_SITES]
+
+ROOT_BODY = "root"
+
+GRAVITY_SENSOR = "upvector"
+GLOBAL_LINVEL_SENSOR = "global_linvel"
+GLOBAL_ANGVEL_SENSOR = "global_angvel"
+LOCAL_LINVEL_SENSOR = "local_linvel"
+ACCELEROMETER_SENSOR = "accelerometer"
+GYRO_SENSOR = "gyro"
