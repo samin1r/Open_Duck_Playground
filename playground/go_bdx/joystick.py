de295d62b28e424ebc0d9c731d8ab36c026a3aa3
apirrone <awanegenebayo@gmail.com>
2025-03-05 13:52:51 +0100

    go_bdx

--- playground/go_bdx/joystick.py
+++ playground/go_bdx/joystick.py
@@ -76,7 +76,7 @@ def default_config() -> config_dict.ConfigDict:
                 tracking_ang_vel=2.5,
                 orientation=-0.5,
                 torques=-1.0e-3,
-                action_rate=-0.375,  # was -1.5
+                action_rate=-1.5,  # was -1.5
                 stand_still=-0.3,  # was -1.0 TODO try to relax this a bit ?
                 alive=20.0,
                 imitation=1.0,
