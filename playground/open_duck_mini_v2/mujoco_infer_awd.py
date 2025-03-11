6b1cbd1ce56983bdca7a3ee3e61ee4d05b5a5599
apirrone <awanegenebayo@gmail.com>
2025-03-11 12:34:11 +0100

    mujoco infer awd

---
+++ playground/open_duck_mini_v2/mujoco_infer_awd.py
@@ -0,0 +1,498 @@
+import mujoco
+import pickle
+import numpy as np
+import mujoco
+import mujoco.viewer
+import time
+import argparse
+from etils import epath
+from playground.common.onnx_infer import OnnxInfer
+
+# from playground.open_duck_mini_v2 import constants
+from playground.open_duck_mini_v2 import base
+
+
+class LowPassActionFilter:
+    def __init__(self, control_freq, cutoff_frequency=30.0):
+        self.last_action = 0
+        self.current_action = 0
+        self.control_freq = float(control_freq)
+        self.cutoff_frequency = float(cutoff_frequency)
+        self.alpha = self.compute_alpha()
+
+    def compute_alpha(self):
+        return (1.0 / self.cutoff_frequency) / (
+            1.0 / self.control_freq + 1.0 / self.cutoff_frequency
+        )
+
+    def push(self, action):
+        self.current_action = action
+
+    def get_filtered_action(self):
+        self.last_action = (
+            self.alpha * self.last_action + (1 - self.alpha) * self.current_action
+        )
+        return self.last_action
+
+
+class MjInfer:
+    def __init__(self, model_path: str, onnx_model_path: str):
+        self.model = mujoco.MjModel.from_xml_string(
+            epath.Path(model_path).read_text(), assets=base.get_assets()
+        )
+
+        # Params
+        self.linearVelocityScale = 1.0
+        self.angularVelocityScale = 1.0
+        self.dof_pos_scale = 1.0
+        self.dof_vel_scale = 0.05
+        self.action_scale = 1.0
+
+        self.obs_history_length = 3
+        self.action_history_length = 3
+        self.obs_size = 40
+        self.action_size = 16
+
+        self.obs_history = np.zeros((self.obs_history_length, self.obs_size))
+        self.action_history = np.zeros((self.action_history_length, self.action_size))
+
+        self.action_clip = [-1.5, 1.5]
+        self.obs_clip = [-5.0, 5.0]
+
+        self.action_filter = LowPassActionFilter(50, 37.5)
+
+        NUM_DOFS = self.model.nu
+        self.floating_base_name = [
+            self.model.jnt(k).name
+            for k in range(0, self.model.njnt)
+            if self.model.jnt(k).type == 0
+        ][
+            0
+        ]  # assuming only one floating object!
+        self.actuator_names = [
+            self.model.actuator(k).name for k in range(0, self.model.nu)
+        ]  # will be useful to get only the actuators we care about
+        self.joint_names = [  # njnt = all joints (including floating base, actuators and backlash joints)
+            self.model.jnt(k).name for k in range(0, self.model.njnt)
+        ]  # all the joint (including the backlash joints)
+        self.backlash_joint_names = [
+            j
+            for j in self.joint_names
+            if j not in self.actuator_names and j not in self.floating_base_name
+        ]  # only the dummy backlash joint
+        self.all_joint_ids = [self.get_joint_id_from_name(n) for n in self.joint_names]
+        self.all_joint_qpos_addr = [
+            self.get_joint_addr_from_name(n) for n in self.joint_names
+        ]
+
+        self.actuator_joint_ids = [
+            self.get_joint_id_from_name(n) for n in self.actuator_names
+        ]
+        self.actuator_joint_qpos_addr = [
+            self.get_joint_addr_from_name(n) for n in self.actuator_names
+        ]
+
+        self.backlash_joint_ids = [
+            self.get_joint_id_from_name(n) for n in self.backlash_joint_names
+        ]
+
+        self.backlash_joint_qpos_addr = [
+            self.get_joint_addr_from_name(n) for n in self.backlash_joint_names
+        ]
+
+        self.all_qvel_addr = np.array(
+            [self.model.jnt_dofadr[jad] for jad in self.all_joint_ids]
+        )
+        self.actuator_qvel_addr = np.array(
+            [self.model.jnt_dofadr[jad] for jad in self.actuator_joint_ids]
+        )
+
+        self.actuator_joint_dict = {
+            n: self.get_joint_id_from_name(n) for n in self.actuator_names
+        }
+
+        self._floating_base_qpos_addr = self.model.jnt_qposadr[
+            np.where(self.model.jnt_type == 0)
+        ][
+            0
+        ]  # Assuming there is only one floating base! the jnt_type==0 is a floating joint. 3 is a hinge
+
+        self._floating_base_qvel_addr = self.model.jnt_dofadr[
+            np.where(self.model.jnt_type == 0)
+        ][
+            0
+        ]  # Assuming there is only one floating base! the jnt_type==0 is a floating joint. 3 is a hinge
+
+        self._floating_base_id = self.model.joint(self.floating_base_name).id
+
+        # self.all_joint_no_backlash_ids=np.zeros(7+self.model.nu)
+        all_idx = self.backlash_joint_ids + list(
+            range(self._floating_base_qpos_addr, self._floating_base_qpos_addr + 7)
+        )
+        all_idx.sort()
+
+        # self.all_joint_no_backlash_ids=[idx for idx in self.all_joint_ids if idx not in self.backlash_joint_ids]+list(range(self._floating_base_add,self._floating_base_add+7))
+        self.all_joint_no_backlash_ids = [idx for idx in all_idx]
+
+        self.model.opt.timestep = 0.002
+        self.data = mujoco.MjData(self.model)
+        mujoco.mj_step(self.model, self.data)
+
+        self.policy = OnnxInfer(onnx_model_path, awd=True)
+
+        self.COMMANDS_RANGE_X = [-0.3, 0.5]
+        self.COMMANDS_RANGE_Y = [-0.3, 0.3]
+        self.COMMANDS_RANGE_THETA = [-1.5, 1.5]  # [-1.0, 1.0]
+
+        self.NECK_PITCH_RANGE = [-0.34, 1.1]
+        self.HEAD_PITCH_RANGE = [-0.78, 0.78]
+        self.HEAD_YAW_RANGE = [-2.7, 2.7]
+        self.HEAD_ROLL_RANGE = [-0.5, 0.5]
+
+        self.last_action = np.zeros(NUM_DOFS)
+        self.last_last_action = np.zeros(NUM_DOFS)
+        self.last_last_last_action = np.zeros(NUM_DOFS)
+        self.commands = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.decimation = 10
+
+        self.init_pos = np.array(
+            self.get_all_joints_qpos(self.model.keyframe("home").qpos)
+        )  # pose of all the joints (no floating base)
+        self.default_actuator = self.model.keyframe(
+            "home"
+        ).ctrl  # ctrl of all the actual joints (no floating base and no backlash)
+
+        # orientation
+        # data.qpos[3 : 3 + 4] = [1, 0, 0.0, 0]
+        # data.qpos[7:] = init_pos
+        self.data.qpos[:] = self.model.keyframe("home").qpos
+        self.data.ctrl[:] = self.default_actuator
+
+        self.gyro_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SENSOR, "gyro")
+        self.gyro_addr = self.model.sensor_adr[self.gyro_id]
+        self.gyro_dimensions = 3
+
+        self.accelerometer_id = mujoco.mj_name2id(
+            self.model, mujoco.mjtObj.mjOBJ_SENSOR, "accelerometer"
+        )
+        self.accelerometer_dimensions = 3
+        self.accelerometer_addr = self.model.sensor_adr[self.accelerometer_id]
+
+        self.linvel_id = mujoco.mj_name2id(
+            self.model, mujoco.mjtObj.mjOBJ_SENSOR, "local_linvel"
+        )
+        self.linvel_dimensions = 3
+
+        self.imu_site_id = mujoco.mj_name2id(
+            self.model, mujoco.mjtObj.mjOBJ_SITE, "imu"
+        )
+
+        self.saved_obs = []
+
+        print(f"joint names: {self.joint_names}")
+        print(f"actuator names: {self.actuator_names}")
+        print(f"backlash joint names: {self.backlash_joint_names}")
+        # print(f"actual joints idx: {self.get_actual_joints_idx()}")
+
+    def get_actuator_id_from_name(self, name: str) -> int:
+        """Return the id of a specified actuator"""
+        return mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_ACTUATOR, name)
+
+    def get_joint_id_from_name(self, name: str) -> int:
+        """Return the id of a specified joint"""
+        return mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_JOINT, name)
+
+    def get_joint_addr_from_name(self, name: str) -> int:
+        """Return the address of a specified joint"""
+        return self.model.joint(name).qposadr
+
+    def get_dof_id_from_name(self, name: str) -> int:
+        """Return the id of a specified dof"""
+        return mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_DOF, name)
+
+    def get_actuator_joint_qpos_from_name(
+        self, data: np.ndarray, name: str
+    ) -> np.ndarray:
+        """Return the qpos of a given actual joint"""
+        addr = self.model.jnt_qposadr[self.actuator_joint_dict[name]]
+        return data[addr]
+
+    def get_actuator_joints_addr(self) -> np.ndarray:
+        """Return the all the idx of actual joints"""
+        addr = np.array(
+            [self.model.jnt_qposadr[idx] for idx in self.actuator_joint_ids]
+        )
+        return addr
+
+    def get_floating_base_qpos(self, data: np.ndarray) -> np.ndarray:
+        return data[self._floating_base_qpos_addr : self._floating_base_qvel_addr + 7]
+
+    def get_floating_base_qvel(self, data: np.ndarray) -> np.ndarray:
+        return data[self._floating_base_qvel_addr : self._floating_base_qvel_addr + 6]
+
+    def set_floating_base_qpos(
+        self, new_qpos: np.ndarray, qpos: np.ndarray
+    ) -> np.ndarray:
+        qpos[self._floating_base_qpos_addr : self._floating_base_qpos_addr + 7] = (
+            new_qpos
+        )
+        return qpos
+
+    def set_floating_base_qvel(
+        self, new_qvel: np.ndarray, qvel: np.ndarray
+    ) -> np.ndarray:
+        qvel[self._floating_base_qvel_addr : self._floating_base_qvel_addr + 6] = (
+            new_qvel
+        )
+        return qvel
+
+    def exclude_backlash_joints_addr(self) -> np.ndarray:
+        """Return the all the idx of actual joints and floating base"""
+        addr = np.array(
+            [self.model.jnt_qposadr[idx] for idx in self.all_joint_no_backlash_ids]
+        )
+        return addr
+
+    def get_all_joints_addr(self) -> np.ndarray:
+        """Return the all the idx of all joints"""
+        addr = np.array([self.model.jnt_qposadr[idx] for idx in self.all_joint_ids])
+        return addr
+
+    def get_actuator_joints_qpos(self, data: np.ndarray) -> np.ndarray:
+        """Return the all the qpos of actual joints"""
+        return data[self.get_actuator_joints_addr()]
+
+    def set_actuator_joints_qpos(
+        self, new_qpos: np.ndarray, qpos: np.ndarray
+    ) -> np.ndarray:
+        """Set the qpos only for the actual joints (omit the backlash joint)"""
+        qpos[self.get_actuator_joints_addr()] = new_qpos
+        return qpos
+
+    def get_actuator_joints_qvel(self, data: np.ndarray) -> np.ndarray:
+        """Return the all the qvel of actual joints"""
+        return data[self.actuator_qvel_addr]
+
+    def set_actuator_joints_qvel(
+        self, new_qvel: np.ndarray, qvel: np.ndarray
+    ) -> np.ndarray:
+        """Set the qvel only for the actual joints (omit the backlash joint)"""
+        qvel[self.actuator_qvel_addr] = new_qvel
+        return qvel
+
+    def get_all_joints_qpos(self, data: np.ndarray) -> np.ndarray:
+        """Return the all the qpos of all joints"""
+        return data[self.get_all_joints_addr()]
+
+    def get_all_joints_qvel(self, data: np.ndarray) -> np.ndarray:
+        """Return the all the qvel of all joints"""
+        return data[self.all_qvel_addr]
+
+    def get_joints_nobacklash_qpos(self, data: np.ndarray) -> np.ndarray:
+        """Return the all the qpos of actual joints with the floating base"""
+        return data[self.exclude_backlash_joints_addr()]
+
+    def set_complete_qpos_from_joints(
+        self, no_backlash_qpos: np.ndarray, full_qpos: np.ndarray
+    ) -> np.ndarray:
+        """In the case of backlash joints, we want to ignore them (remove them) but we still need to set the complete state incuding them"""
+        full_qpos[self.exclude_backlash_joints_addr()] = no_backlash_qpos
+        return np.array(full_qpos)
+
+    def get_sensor(self, data, name, dimensions):
+        i = self.model.sensor_name2id(name)
+        return data.sensordata[i : i + dimensions]
+
+    def get_gyro(self, data):
+        return data.sensordata[self.gyro_addr : self.gyro_addr + self.gyro_dimensions]
+
+    def get_accelerometer(self, data):
+        return data.sensordata[
+            self.accelerometer_addr : self.accelerometer_addr
+            + self.accelerometer_dimensions
+        ]
+
+    def get_linvel(self, data):
+        return data.sensordata[self.linvel_id : self.linvel_id + self.linvel_dimensions]
+
+    def get_gravity(self, data):
+        return data.site_xmat[self.imu_site_id].reshape((3, 3)).T @ np.array([0, 0, -1])
+
+    def check_contact(self, data, body1_name, body2_name):
+        body1_id = data.body(body1_name).id
+        body2_id = data.body(body2_name).id
+
+        for i in range(data.ncon):
+            try:
+                contact = data.contact[i]
+            except Exception as e:
+                return False
+
+            if (
+                self.model.geom_bodyid[contact.geom1] == body1_id
+                and self.model.geom_bodyid[contact.geom2] == body2_id
+            ) or (
+                self.model.geom_bodyid[contact.geom1] == body2_id
+                and self.model.geom_bodyid[contact.geom2] == body1_id
+            ):
+                return True
+
+        return False
+
+    def get_feet_contacts(self, data):
+        left_contact = self.check_contact(data, "foot_assembly", "floor")
+        right_contact = self.check_contact(data, "foot_assembly_2", "floor")
+        return left_contact, right_contact
+
+    def get_obs(
+        self,
+        data,
+        last_action,
+        command,  # , qvel_history, qpos_error_history, gravity_history
+    ):
+        gyro = self.get_gyro(data)
+        accelerometer = self.get_accelerometer(data)
+
+        gravity = self.get_gravity(data)
+        joint_angles = self.get_actuator_joints_qpos(data.qpos)
+        joint_vel = self.get_actuator_joints_qvel(data.qvel)
+
+        contacts = self.get_feet_contacts(data)
+
+        # obs = np.concatenate(
+        #     [
+        #         self.projected_gravity,  # 3
+        #         (self.joint_positions - self.init_pos) * self.joint_pos_scale,  # 16
+        #         self.joint_velocities * self.joint_vel_scale,  # 16
+        #         self.angular_velocity * self.angular_vel_scale_obs,  # 3
+        #         self.feet_contact,  # 2
+        #     ]
+        # )
+        obs = np.concatenate(
+            [
+                gravity,
+                # command[:3],
+                self.add_fake_antennas(joint_angles - self.default_actuator),
+                self.add_fake_antennas(joint_vel),
+                gyro,
+                contacts,
+            ]
+        )
+
+        self.obs_history[1:, :] = self.obs_history[:-1, :].copy()
+        self.obs_history[0, : len(obs)] = obs
+
+        obs = np.concatenate(
+            [self.obs_history.flatten(), self.action_history.flatten(), np.array(command[:3])]
+        ).reshape(1, -1)
+        obs = np.clip(obs, self.obs_clip[0], self.obs_clip[1])
+
+        return obs
+
+    def key_callback(self, keycode):
+        print(f"key: {keycode}")
+        lin_vel_x = 0
+        lin_vel_y = 0
+        ang_vel = 0
+
+        if keycode == 265:  # arrow up
+            lin_vel_x = self.COMMANDS_RANGE_X[1]
+        if keycode == 264:  # arrow down
+            lin_vel_x = self.COMMANDS_RANGE_X[0]
+        if keycode == 263:  # arrow left
+            lin_vel_y = self.COMMANDS_RANGE_Y[1]
+        if keycode == 262:  # arrow right
+            lin_vel_y = self.COMMANDS_RANGE_Y[0]
+        if keycode == 81:  # a
+            ang_vel = self.COMMANDS_RANGE_THETA[1]
+        if keycode == 69:  # e
+            ang_vel = self.COMMANDS_RANGE_THETA[0]
+
+        self.commands[0] = lin_vel_x
+        self.commands[1] = lin_vel_y
+        self.commands[2] = ang_vel
+
+    def add_fake_antennas(self, pos):
+        pos_with_antennas = np.insert(pos, 9, [0, 0])
+        return np.array(pos_with_antennas)
+
+    def remove_fake_antennas(self, pos):
+        return np.delete(pos, [9, 10])
+
+
+
+    def run(self):
+        try:
+            with mujoco.viewer.launch_passive(
+                self.model,
+                self.data,
+                show_left_ui=False,
+                show_right_ui=False,
+                key_callback=self.key_callback,
+            ) as viewer:
+                counter = 0
+                while True:
+
+                    step_start = time.time()
+
+                    mujoco.mj_step(self.model, self.data)
+
+                    counter += 1
+
+                    if counter % self.decimation == 0:
+                        obs = self.get_obs(
+                            self.data,
+                            self.last_action,
+                            self.commands,
+                        )
+                        self.saved_obs.append(obs)
+                        action = self.policy.infer(obs.flatten())
+                        action = np.clip(
+                            action, self.action_clip[0], self.action_clip[1]
+                        )
+
+                        self.action_history[1:, :] = self.action_history[:-1, :].copy()
+                        self.action_history[0, :] = action.copy()
+
+                        action[[5, 6, 7, 8]] = 0.0
+                        action = self.remove_fake_antennas(action)
+
+                        action = self.default_actuator + action * self.action_scale 
+
+                        self.action_filter.push(action)
+                        filtered_action = self.action_filter.get_filtered_action()
+
+                        self.data.ctrl = filtered_action.copy()
+
+                    viewer.sync()
+
+                    time_until_next_step = self.model.opt.timestep - (
+                        time.time() - step_start
+                    )
+                    if time_until_next_step > 0:
+                        time.sleep(time_until_next_step)
+        except KeyboardInterrupt:
+            pickle.dump(self.saved_obs, open("mujoco_saved_obs.pkl", "wb"))
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--onnx_model_path", type=str, required=True)
+    # parser.add_argument("-k", action="store_true", default=False)
+    parser.add_argument(
+        "--reference_data",
+        type=str,
+        default="playground/open_duck_mini_v2/data/polynomial_coefficients.pkl",
+    )
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        default="playground/open_duck_mini_v2/xmls/scene_mjx_flat_terrain.xml",
+    )
+    parser.add_argument("--standing", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    mjinfer = MjInfer(args.model_path, args.onnx_model_path)
+    mjinfer.run()
