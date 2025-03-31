142c1b384da518e3b509c96a84ce291c174747ce
MankaranSingh <mankaran32@gmail.com>
2025-03-31 15:24:35 +0530

    more head_pos cost

--- playground/open_duck_mini_v2/joystick.py
+++ playground/open_duck_mini_v2/joystick.py
@@ -84,7 +84,7 @@ def default_config() -> config_dict.ConfigDict:
                 stand_still=-0.05,  # was -1.0 TODO try to relax this a bit ?
                 alive=20.0,
                 imitation=1.0,
-                head_pos=2.0,
+                head_pos=10.0,
             ),
             tracking_sigma=0.01,  # was working at 0.01
         ),
