bb8ff80735913146711adbbf87b7f8cd3d8ec427
apirrone <awanegenebayo@gmail.com>
2025-04-19 15:51:39 +0200

    update

--- playground/sigmaban2024/joystick.py
+++ playground/sigmaban2024/joystick.py
@@ -96,10 +96,10 @@ def default_config() -> config_dict.ConfigDict:
         ),
         push_config=config_dict.create(
             enable=True,
-            interval_range=[2.0, 5.0],
-            magnitude_range=[0.1, 5.0],
+            interval_range=[5.0, 10.0],
+            magnitude_range=[0.1, 1.0],
         ),
-        lin_vel_x=[-0.15, 0.15],
+        lin_vel_x=[-0.3, 0.3],
         lin_vel_y=[-0.2, 0.2],
         ang_vel_yaw=[-1.0, 1.0],  # [-1.0, 1.0]
         neck_pitch_range=[-0.34, 1.1],
