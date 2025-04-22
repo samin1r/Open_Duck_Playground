0b3b2b54144e261ba2d2c4622f08aa1c6f4821f9
apirrone <awanegenebayo@gmail.com>
2025-04-22 14:42:19 +0200

    more push

--- playground/sigmaban2024/joystick.py
+++ playground/sigmaban2024/joystick.py
@@ -97,7 +97,7 @@ def default_config() -> config_dict.ConfigDict:
         push_config=config_dict.create(
             enable=True,
             interval_range=[5.0, 10.0],
-            magnitude_range=[0.1, 1.0],
+            magnitude_range=[0.1, 5.0],
         ),
         lin_vel_x=[-0.15, 0.15],
         lin_vel_y=[-0.2, 0.2],
